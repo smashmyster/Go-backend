a958ef8c863f95806486ea0d09975b668ce47bf2
Jackson <dyorajackson@gmail.com>
2024-04-26 12:19:32 +0200

    path resolving

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,6 @@ import { ChatController } from './app/logic/chat/chat.controller';
 import { ChatService } from './app/logic/chat/chat.service';
 import { ChatModule } from './app/logic/chat/chat.module';
 import { Gateway } from './app/logic/gateway/gateway';
-import { GatewayModule } from './app/logic/gateway/gateway.module';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 @Module({
@@ -46,7 +45,6 @@ require('dotenv').config();
     AuthModule,
     RequestsModule,
     ChatModule,
-    GatewayModule,
   ],
   controllers: [
     AppController,
