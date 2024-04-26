e9e6bd7abc0e2bbeebeec4dec8be27a65f025448
Jackson <dyorajackson@gmail.com>
2024-04-26 12:16:37 +0200

    path resolving

--- src/app/logic/gender/gender.service.ts
+++ src/app/logic/gender/gender.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Gender } from '../../entities/Gender';
+import { Gender } from '../../entities/gender';
 
 @Injectable()
 export class GenderService {
